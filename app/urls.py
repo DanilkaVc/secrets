3f2af855e8e69e52184989761f0a381085fd0bb0
DanilkaVC <danilabondarenko312@gmail.com>
2025-05-05 17:57:40 +0300

    minor changes

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,6 @@ from firstapp import views
 from django.urls import path, re_path
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    re_path('about/*', views.about, name='about', kwargs={'name': 'tom', "age": 100}),
-    re_path('support/*', views.support, name='support'),
+    
 ]
 

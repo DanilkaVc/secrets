6f31ab2ad5a4b95d5f292a63e137a34e196179c9
DanilkaVc <danilabondarenko312@gmail.com>
2025-05-05 17:36:46 +0300

    start development

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,7 @@
-from firstapp import views
+from backend import views
 from django.urls import path, re_path
 
 urlpatterns = [
     path('', views.index, name='home'),
-    re_path('about/*', views.about, name='about', kwargs={'name': 'tom', "age": 100}),
-    re_path('support/*', views.support, name='support'),
 ]
 
